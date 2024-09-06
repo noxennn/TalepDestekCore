5147acc64b8a836a39fa09f54e1624a683ed451f
Yusuf Emre Bayrakcı <69535693+noxennn@users.noreply.github.com>
2024-09-06 16:03:20 +0300

    Add project files.

---
+++ TalepDestekCore/Areas/Student/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TalepDestekCore.Areas.Student.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Area("Student")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
